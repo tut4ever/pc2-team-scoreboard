81462b770501bb310816d2b9c04c7b98e8a5eb72
randomnameftw <randomnameftw@62af5674-1c6c-bfd3-718a-20af09b1249b>
2011-01-17 00:33:15 +0000

    made a connection report parser

---
+++ trunk/PC2 Scoreboard/src/cs/clements/pc2/scoreboard/Parser.java
@@ -0,0 +1,39 @@
+package cs.clements.pc2.scoreboard;
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.io.File;
+
+public class Parser 
+{
+	HashMap<String,String> connections = new HashMap<String,String>();
+	
+	public static void main(String...args)
+	{
+		System.out.println(new Parser("connections"));	
+	}
+	
+	public Parser(String file)
+	{
+		Scanner in = null;
+		try{in = new Scanner(new File(file));}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+			System.out.println("File not found");
+		}
+		while(!in.nextLine().contains("TEAM")){}
+		
+		String connection = null;
+		
+		while((connection=in.nextLine()).contains("   TEAM"))
+		{
+			connections.put(connection.split(" ")[3],connection.split(" ")[8].substring(13,22));
+		}
+	}
+	public String toString()
+	{
+		return connections.toString();
+	}
+	
+}
